7cc0b02f7661f091621a63684f41a2243c87b668
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2023-02-15 04:37:58 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+let navbar = document.querySelector('.header .navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+};
+
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+registerBtn.onclick = () =>{
+  registerBtn.classList.add('active');
+  loginBtn.classList.remove('active');
+  document.querySelector('.account-form .login-form').classList.remove('active');
+  document.querySelector('.account-form .register-form').classList.add('active');
+};
+
+loginBtn.onclick = () =>{
+  registerBtn.classList.remove('active');
+  loginBtn.classList.add('active');
+  document.querySelector('.account-form .login-form').classList.add('active');
+  document.querySelector('.account-form .register-form').classList.remove('active');
+};
+
+let accountForm = document.querySelector('.account-form')
+
+document.querySelector('#account-btn').onclick = () =>{
+  accountForm.classList.add('active');
+}
+
+document.querySelector('#close-form').onclick = () =>{
+  accountForm.classList.remove('active');
+};
+
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+  acco.onclick = () =>{
+    accordion.forEach(dion => dion.classList.remove('active'));
+    acco.classList.toggle('active');
+  };
+});
+
+
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    var name = document.forms['myForm']["fname"].value;
+    if (name.length<2){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)){
+      seterror("name","*Only characters are allowed");
+    }
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    var email = document.forms['myForm']["femail"].value;
+    if (email.length>35){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+
+    var phone = document.forms['myForm']["fphone"].value;
+    if (phone.length != 10){
+        seterror("phone", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+       seterror("phone","*Only digits please");
+       returnval=false;
+    }
+    return returnval;
+}
+
+
